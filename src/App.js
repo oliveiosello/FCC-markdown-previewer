ebbcc2e4950e8f64e6bef9e41c0149a8a5bc884e
Olive Iosello <oiosello@gmail.com>
2020-10-23 12:28:19 -0700

    Made all tests pass.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,133 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Badge from 'react-bootstrap/Badge';
+let marked = require('marked');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const defaultMD = `# HOWDY! 
+## welcome to this fancy markdown previewer
+[freeCodeCamp](https://www.freecodecamp.org/oiosello)
+Some things I used for this project:
+- React
+- Bootstrap
+- Marked
+
+Look what I can do: 
+**bold**, _emphasis_ , _**combined emphasis**_ , and ~~strikethrough~~
+
+Head 1 | Head 2 | Head 3
+------ |--------| ------
+item1 | item2 | item3
+item4 | item5 | item6
+item7 | item8 | item9
+
+> example of blockquote
+Inline \`code\`
+\`\`\`
+// code block
+function bowie (alien, angel) {
+  return alien + angel;
+}
+\`\`\`
+![david bowie](https://i.pinimg.com/474x/88/18/ec/8818ec960e8721f33d6a1f21458b6419.jpg)
+`;
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      md: defaultMD,
+    };
+  }
+
+  handleMd(md) {
+    this.setState({ md });
+  }
+
+  render() {
+    const pageStyle = {
+      backgroundColor: 'purple'
+    };
+
+    const titleStyle = {
+      backgroundColor: 'transparent',
+      color: 'white',
+      
+    }
+
+    const subtitleStyle = {
+      backgroundColor: 'transparent',
+      color: 'white'
+    }
+
+    const inputStyle = {
+      width: '350px',
+      height: '350px',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      padding: '10px',
+    };
+
+    const outputStyle = {
+      width: '350px',
+      height: 'auto',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      padding: '10px',
+      backgroundColor: 'plum',
+    };
+
+    return (
+      <div className="App" style={pageStyle}>
+        <div className="container">
+          <div className="row mt-4">
+            <div className="col text-center">
+              <h1>
+                <Badge className="text-align-center" style={titleStyle}>
+                  MARKDOWN PREVIEWER
+                </Badge>
+              </h1>
+            </div>
+          </div>
+          <div className="row mt-4">
+            <div className="col-md-6">
+              <div className="col text-center">
+                <h4>
+                  <Badge className="text-align-center" style={subtitleStyle}>
+                    input
+                  </Badge>
+                </h4>
+              </div>
+              <div className="mark-input" style={inputStyle}>
+                <textarea
+                  id="editor"
+                  className="input"
+                  style={inputStyle}
+                  value={this.state.md}
+                  onChange={(e) => {
+                    this.handleMd(e.target.value);
+                  }}
+                ></textarea>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="col text-center">
+                <h4>
+                  <Badge className="text-align-center" style={subtitleStyle}>
+                    output
+                  </Badge>
+                </h4>
+              </div>
+              <div
+                id="preview"
+                style={outputStyle}
+                dangerouslySetInnerHTML={{
+                  __html: marked(this.state.md),
+                }}
+              ></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
